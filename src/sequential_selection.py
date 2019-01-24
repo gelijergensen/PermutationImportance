93d4360fedad0480f74f2d501e8f12c4410b830a
G. Eli Jergensen <gelijergensen@ou.edu>
2019-01-24 11:49:36 -0700

    Added sequential forward selection and simple test (#38)

--- src/sequential_selection.py
+++ src/sequential_selection.py
@@ -1,55 +1,74 @@
-"""While there are slightly different strategies for performing sequential 
+"""While there are slightly different strategies for performing sequential
 selection, they all use the same base idea, which is represented here"""
 
+import numpy as np
 
 from src.data_verification import verify_data, determine_variable_names
 from src.result import ImportanceResult
+from src.selection_strategies import sfs_strategy
 from src.scoring_strategies import verify_scoring_strategy
 from src.utils import convert_result_list_to_dict, get_data_subset
 
 
-def sequential_selection(training_data, scoring_data, scoring_fn, scoring_strategy, selection_strategy, variable_names=None, nimportant_vars=None):
-    """Performs an abstract sequential selection over data given a particular 
+__all__ = ["sequential_forward_selection"]
+
+
+def sequential_selection(training_data, scoring_data, scoring_fn, scoring_strategy, selection_strategy, variable_names=None, nimportant_vars=None, method=None):
+    """Performs an abstract sequential selection over data given a particular
     set of functions for scoring, determining optimal variables, and selecting
     data
 
-    :param training_data: a 2-tuple (inputs, outputs) for training in the 
+    :param training_data: a 2-tuple (inputs, outputs) for training in the
         scoring_fn
-    :param scoring_data: a 2-tuple (inputs, outputs) for scoring in the 
+    :param scoring_data: a 2-tuple (inputs, outputs) for scoring in the
         scoring_fn
-    :param scoring_fn: a function to be used for scoring. Should be of the form 
+    :param scoring_fn: a function to be used for scoring. Should be of the form
         (training_data, scoring_data) -> float
-    :param scoring_strategy: a function to be used for determining optimal 
-        variables. Should be of the form ([floats]) -> index
+    :param scoring_strategy: a function to be used for determining optimal
+        variables or a string. If a function, should be of the form
+            ([floats]) -> index. If a string, must be one of the options in
+        scoring_strategies.VALID_SCORING_STRATEGIES
     :param selection_strategy: a function to be used for selecting a fraction of
-        the data to be used at each iteration. Should be of the form 
-        (num_vars, important_vars: [index]) -> 
+        the data to be used at each iteration. Should be of the form
+        (num_vars, important_vars: [index]) ->
             list of (variable being evaluated, data columns to include)
-    :param variable_names: an optional list for variable names. If not given, 
-        will use names of columns of data (if pandas dataframe) or column 
+    :param variable_names: an optional list for variable names. If not given,
+        will use names of columns of data (if pandas dataframe) or column
         indices
     :param nimportant_vars: number of times to compute the next most important
         variable. Defaults to all
+    :param method: a string for the name of the method used. Defaults to the
+        name of the selection_strategy if not given
     :returns: ImportanceResult object which contains the results for each run
     """
 
     training_data = verify_data(training_data)
     scoring_data = verify_data(scoring_data)
 
+    scoring_strategy = verify_scoring_strategy(scoring_strategy)
+
     variable_names = determine_variable_names(training_data, variable_names)
 
     nimportant_vars = len(
         variable_names) if nimportant_vars is None else nimportant_vars
 
-    result_obj = ImportanceResult("FILL_THE_METHOD_IN_LATER", variable_names)
+    method = selection_strategy.__name__ if method is None else method
+
+    result_obj = ImportanceResult(method, variable_names)
 
     important_vars = list()
     num_vars = len(variable_names)
     for _ in range(nimportant_vars):
         result = _singlethread_iteration(training_data, scoring_data, scoring_fn,
                                          selection_strategy(num_vars, important_vars))
+        next_result = convert_result_list_to_dict(
+            result, variable_names, scoring_strategy)
+        best_var = min(
+            next_result.keys(), key=lambda key: next_result[key][0])
+        best_index = np.flatnonzero(variable_names == best_var)[0]
         result_obj.add_new_results(
-            convert_result_list_to_dict(result, variable_names, scoring_strategy))
+            next_result, next_important_variable=best_var)
+        important_vars.append(best_index)
 
     return result_obj
 
@@ -60,7 +79,7 @@ def _singlethread_iteration(training_data, scoring_data, scoring_fn, selection_i
 
     :param training_data: (training_inputs, training_outputs)
     :param scoring_data: (scoring_inputs, scoring_outputs)
-    :param scoring_fn: a function to be used for scoring. Should be of the form 
+    :param scoring_fn: a function to be used for scoring. Should be of the form
         (training_data, scoring_data) -> float
     :param selection_iterator: the result of the selection strategy. Should be
         an iterable of (variable being evaluated, data columns to include)
@@ -79,3 +98,26 @@ def _singlethread_iteration(training_data, scoring_data, scoring_fn, selection_i
                            (scoring_inputs_subset, scoring_outputs))
         result.append((var, score))
     return result
+
+
+def sequential_forward_selection(training_data, scoring_data, scoring_fn, scoring_strategy, variable_names=None, nimportant_vars=None):
+    """Performs an abstract sequential selection over data given a particular
+    set of functions for scoring, determining optimal variables, and selecting
+    data
+
+    :param training_data: a 2-tuple (inputs, outputs) for training in the
+        scoring_fn
+    :param scoring_data: a 2-tuple (inputs, outputs) for scoring in the
+        scoring_fn
+    :param scoring_fn: a function to be used for scoring. Should be of the form
+        (training_data, scoring_data) -> float
+    :param scoring_strategy: a function to be used for determining optimal
+        variables. Should be of the form ([floats]) -> index
+    :param variable_names: an optional list for variable names. If not given,
+        will use names of columns of data (if pandas dataframe) or column
+        indices
+    :param nimportant_vars: number of times to compute the next most important
+        variable. Defaults to all
+    :returns: ImportanceResult object which contains the results for each run
+    """
+    return sequential_selection(training_data, scoring_data, scoring_fn, scoring_strategy, sfs_strategy, variable_names=variable_names, nimportant_vars=nimportant_vars, method="Sequential Forward Selection")
--- test/test_sequential_selection.py
+++ test/test_sequential_selection.py
@@ -2,7 +2,8 @@
 
 import pandas as pd
 
-from src.sequential_selection import _singlethread_iteration
+from src.result import ImportanceResult
+from src.sequential_selection import _singlethread_iteration, sequential_forward_selection
 
 
 def test__singlethread_iteration():
@@ -23,3 +24,38 @@ def test__singlethread_iteration():
     expected = [(0, 1), (2, 3)]
     assert expected == _singlethread_iteration(training_data, scoring_data,
                                                scoring_fn, selection_iterator)
+
+
+def test_sequential_forward_selection():
+    A = [1, 2]
+    B = [2, 4]
+    C = [3, 6]
+    D = [1, 0]
+    inputs = pd.DataFrame({'A': A, 'B': B, 'C': C})
+    outputs = pd.DataFrame({'D': D})
+
+    training_data = (inputs, outputs)
+    scoring_data = (inputs, outputs)
+
+    def scoring_fn(training_data, scoring_data):
+        if 'A' in training_data[0].columns:
+            return scoring_data[0].iloc[1, -1]
+        else:
+            return scoring_data[0].iloc[1, -1] / 2
+
+    expected = ImportanceResult(
+        "Sequential Forward Selection", ["A", "B", "C"])
+    expected.add_new_results({'A': (0, 2), 'B': (1, 2), 'C': (2, 3)})
+    expected.add_new_results({'B': (0, 4), 'C': (1, 6)})
+    expected.add_new_results({'C': (0, 6)})
+
+    result = sequential_forward_selection(
+        training_data, scoring_data, scoring_fn, "argmin")
+
+    assert expected.method == result.method
+    assert (expected.variable_names == result.variable_names).all()
+    assert expected.retrieve_breiman() == result.retrieve_breiman()
+    assert expected.retrieve_laks() == result.retrieve_laks()
+    for (exp_context, exp_result), (true_context, true_result) in zip(expected, result):
+        assert exp_context == true_context
+        assert exp_result == true_result
