2075195021667916768f55043cf05fcf33d1013f
G. Eli Jergensen <gelijergensen@ou.edu>
2019-01-29 15:23:41 -0600

    Added Sequential Backward Selection

--- src/sequential_selection.py
+++ src/sequential_selection.py
@@ -7,12 +7,12 @@ import multiprocessing as mp
 
 from src.data_verification import verify_data, determine_variable_names
 from src.result import ImportanceResult
-from src.selection_strategies import SequentialForwardSelectionStrategy
+from src.selection_strategies import SequentialForwardSelectionStrategy, SequentialBackwardSelectionStrategy
 from src.scoring_strategies import verify_scoring_strategy
 from src.utils import add_ranks_to_dict, get_data_subset
 
 
-__all__ = ["sequential_forward_selection"]
+__all__ = ["sequential_forward_selection", "sequential_backward_selection"]
 
 
 def sequential_selection(training_data, scoring_data, scoring_fn, scoring_strategy, selection_strategy, variable_names=None, nimportant_vars=None, method=None, nbootstrap=1, subsample=1, njobs=1):
@@ -166,9 +166,8 @@ def _multithreaded_runner(in_queue, out_queue, func):
 
 
 def sequential_forward_selection(training_data, scoring_data, scoring_fn, scoring_strategy, variable_names=None, nimportant_vars=None, nbootstrap=1, subsample=1, njobs=1):
-    """Performs an abstract sequential selection over data given a particular
-    set of functions for scoring, determining optimal variables, and selecting
-    data
+    """Performs sequential forward selection over data given a particular
+    set of functions for scoring and determining optimal variables
 
     : param training_data: a 2-tuple(inputs, outputs) for training in the
         scoring_fn
@@ -195,3 +194,34 @@ def sequential_forward_selection(training_data, scoring_data, scoring_fn, scorin
     : returns: ImportanceResult object which contains the results for each run
     """
     return sequential_selection(training_data, scoring_data, scoring_fn, scoring_strategy, SequentialForwardSelectionStrategy, variable_names=variable_names, nimportant_vars=nimportant_vars, nbootstrap=nbootstrap, subsample=subsample, njobs=njobs)
+
+
+def sequential_backward_selection(training_data, scoring_data, scoring_fn, scoring_strategy, variable_names=None, nimportant_vars=None, nbootstrap=1, subsample=1, njobs=1):
+    """Performs sequential backward selection over data given a particular
+    set of functions for scoring and determining optimal variables
+
+    : param training_data: a 2-tuple(inputs, outputs) for training in the
+        scoring_fn
+    : param scoring_data: a 2-tuple(inputs, outputs) for scoring in the
+        scoring_fn
+    : param scoring_fn: a function to be used for scoring. Should be of the form
+        (training_data, scoring_data) -> float
+    : param scoring_strategy: a function to be used for determining optimal
+        variables. Should be of the form([floats]) -> index
+    : param variable_names: an optional list for variable names. If not given,
+        will use names of columns of data(if pandas dataframe) or column
+        indices
+    : param nimportant_vars: number of times to compute the next most important
+        variable. Defaults to all
+    : param nbootstrap: number of times to perform scoring on each variable.
+        Results over different bootstrap iterations are averaged. Defaults to 1
+    : param subsample: number of elements to sample(with replacement) per
+        bootstrap round. If between 0 and 1, treated as a fraction of the number
+        of total number of events(e.g. 0.5 means half the number of events).
+        If not specified, subsampling will not be used and the entire data will
+        be used(without replacement)
+    : param njobs: an integer for the number of threads to use. If negative, will
+        use the number of cpus + njobs. Defaults to 1
+    : returns: ImportanceResult object which contains the results for each run
+    """
+    return sequential_selection(training_data, scoring_data, scoring_fn, scoring_strategy, SequentialBackwardSelectionStrategy, variable_names=variable_names, nimportant_vars=nimportant_vars, nbootstrap=nbootstrap, subsample=subsample, njobs=njobs)
--- test/test_sequential_selection.py
+++ test/test_sequential_selection.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 from src.result import ImportanceResult
-from src.sequential_selection import _singlethread_iteration, _multithread_iteration, sequential_forward_selection
+from src.sequential_selection import _singlethread_iteration, _multithread_iteration, sequential_forward_selection, sequential_backward_selection
 
 
 def test__singlethread_iteration():
@@ -66,6 +66,43 @@ def test_sequential_forward_selection():
     expected.add_new_results({'C': (0, 6)})
 
     result = sequential_forward_selection(
+        training_data, scoring_data, scoring_fn, "argmin", nbootstrap=2)
+
+    assert expected.method == result.method
+    assert (expected.variable_names == result.variable_names).all()
+    assert expected.retrieve_breiman() == result.retrieve_breiman()
+    assert expected.retrieve_laks() == result.retrieve_laks()
+    for (exp_context, exp_result), (true_context, true_result) in zip(expected, result):
+        assert exp_context == true_context
+        assert exp_result == true_result
+
+
+def test_sequential_backward_selection():
+    A = [1, 2]
+    B = [2, 4]
+    C = [3, 6]
+    D = [1, 0]
+    inputs = pd.DataFrame({'A': A, 'B': B, 'C': C})
+    outputs = pd.DataFrame({'D': D})
+
+    training_data = (inputs, outputs)
+    scoring_data = (inputs, outputs)
+
+    def scoring_fn(training_data, scoring_data):
+        if len(training_data[0].columns) == 0:
+            return 0
+        elif 'A' not in training_data[0].columns:
+            return scoring_data[0].iloc[1, 0]
+        else:
+            return scoring_data[0].iloc[1, 0] / 2
+
+    expected = ImportanceResult(
+        "Sequential Backward Selection", ["A", "B", "C"])
+    expected.add_new_results({'A': (2, 4), 'B': (0, 1), 'C': (1, 1)})
+    expected.add_new_results({'A': (1, 6), 'C': (0, 1)})
+    expected.add_new_results({'A': (0, 0)})
+
+    result = sequential_backward_selection(
         training_data, scoring_data, scoring_fn, "argmin", nbootstrap=2)
 
     assert expected.method == result.method
