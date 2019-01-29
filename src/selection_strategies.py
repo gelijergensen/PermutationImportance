ddd617b6320af12e8d465988545162d8c71915d7
G. Eli Jergensen <gelijergensen@ou.edu>
2019-01-29 14:27:10 -0700

    Added Sequential Backward Selection (#46)

--- src/selection_strategies.py
+++ src/selection_strategies.py
@@ -3,7 +3,8 @@ import numpy as np
 
 from src.utils import get_data_subset
 
-__all__ = ["SequentialForwardSelectionStrategy"]
+__all__ = ["SequentialForwardSelectionStrategy",
+           "SequentialBackwardSelectionStrategy"]
 
 
 class SelectionStrategy(object):
@@ -50,12 +51,7 @@ class SequentialForwardSelectionStrategy(SelectionStrategy):
         """Check each of the non-important variables. Dataset is the columns which
         are important plus the one being evaluated
 
-        :param num_vars: integer for the total number of variables
-        :param important_vars: a list of the indices of variables which are already
-            considered important
-        :param bootstrap_iter: ignored
-        :param subsample: ignored
-        :returns: a list of (variable being evaluated, columns to include)
+        :yields: a sequence of (variable being evaluated, columns to include)
         """
         training_inputs, training_outputs = self.training_data
         scoring_inputs, scoring_outputs = self.scoring_data
@@ -72,6 +68,45 @@ class SequentialForwardSelectionStrategy(SelectionStrategy):
         for var in range(self.num_vars):
             if var not in self.important_vars:
                 columns = self.important_vars + [var, ]
+                # Make a slice of the training data
+                training_inputs_subset = get_data_subset(
+                    training_inputs, train_rows, columns)
+                training_outputs_subset = get_data_subset(
+                    training_outputs, train_rows)
+                # Make a slice of the scoring data
+                scoring_inputs_subset = get_data_subset(
+                    scoring_inputs, all_score_rows, columns)
+                yield((var, (training_inputs_subset, training_outputs_subset), (scoring_inputs_subset, scoring_outputs)))
+
+
+class SequentialBackwardSelectionStrategy(SelectionStrategy):
+    """Sequential Backward Selection tests each variable and attempts to remove
+    one new variable from the dataset on each iteration"""
+
+    name = "Sequential Backward Selection"
+
+    def generate_datasets(self):
+        """Check each of the non-important variables. Dataset is the columns which
+        are not important minus the one being evaluated
+
+        :yields: a sequence of (variable being evaluated, columns to include)
+        """
+        training_inputs, training_outputs = self.training_data
+        scoring_inputs, scoring_outputs = self.scoring_data
+
+        if self.subsample == training_inputs.shape[0]:
+            # Deterministic
+            train_rows = np.arange(training_inputs.shape[0])
+        else:
+            # First we randomly pick the examples for training
+            train_rows = np.random.choice(
+                training_inputs.shape[0], self.subsample)
+        all_score_rows = np.arange(scoring_inputs.shape[0])
+
+        for var in range(self.num_vars):
+            if var not in self.important_vars:
+                columns = [x for x in range(self.num_vars) if x not in (
+                    self.important_vars + [var, ])]
                 # Make a slice of the training data
                 training_inputs_subset = get_data_subset(
                     training_inputs, train_rows, columns)
--- test/test_selection_strategies.py
+++ test/test_selection_strategies.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pytest
 
-from src.selection_strategies import SequentialForwardSelectionStrategy, SelectionStrategy
+from src.selection_strategies import SequentialForwardSelectionStrategy, SequentialBackwardSelectionStrategy, SelectionStrategy
 
 
 def test_selection_strategy():
@@ -32,6 +32,41 @@ def test_sfs_strategy():
 
     expected = [(0, (training_data[0][:, [1, 0]], training_data[1]), (scoring_data[0][:, [1, 0]], scoring_data[1])),
                 (2, (training_data[0][:, [1, 2]], training_data[1]), (scoring_data[0][:, [1, 2]], scoring_data[1]))]
+    for (exp_var, exp_train_data, exp_score_data), (res_var, res_train_data, res_score_data) in zip(expected, strategy):
+        assert exp_var == res_var
+        assert (exp_train_data[0] == res_train_data[0]).all()
+        assert (exp_train_data[1] == res_train_data[1]).all()
+        assert (exp_score_data[0] == res_score_data[0]).all()
+        assert (exp_score_data[1] == res_score_data[1]).all()
+
+    subsample = 3
+    strategy = SequentialForwardSelectionStrategy(
+        training_data, scoring_data, num_vars, important_vars, bootstrap_iter, subsample)
+
+    for (res_var, res_train_data, res_score_data) in strategy:
+        assert len(res_train_data[0]) == subsample
+        assert len(res_train_data[1]) == subsample
+        assert len(res_score_data[0]) == len(scoring_data[0])
+        assert len(res_score_data[1]) == len(scoring_data[1])
+
+
+def test_sbs_strategy():
+
+    training_data = (np.random.rand(5, 3), np.random.rand(5, ))
+    scoring_data = (np.random.rand(5, 3), np.random.rand(5, ))
+
+    subsample = 5
+    bootstrap_iter = 0
+    num_vars = training_data[0].shape[1]
+    important_vars = [1]
+
+    strategy = SequentialBackwardSelectionStrategy(
+        training_data, scoring_data, num_vars, important_vars, bootstrap_iter, subsample)
+
+    assert getattr(strategy, "name") == "Sequential Backward Selection"
+
+    expected = [(0, (training_data[0][:, [2]], training_data[1]), (scoring_data[0][:, [2]], scoring_data[1])),
+                (2, (training_data[0][:, [0]], training_data[1]), (scoring_data[0][:, [0]], scoring_data[1]))]
     for (exp_var, exp_train_data, exp_score_data), (res_var, res_train_data, res_score_data) in zip(expected, strategy):
         assert exp_var == res_var
         assert (exp_train_data[0] == res_train_data[0]).all()
