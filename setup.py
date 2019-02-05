7e8489f5e298bda91ac16758bcf81fcab1457805
G. Eli Jergensen <gelijergensen@ou.edu>
2019-02-05 11:12:26 -0600

    Fixed setup.py to know the new location of the code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-PACKAGE_NAMES = ['permutation_importance']
+PACKAGE_NAMES = ['src']
 KEYWORDS = [
     'variable importance', 'model evaluation']
 SHORT_DESCRIPTION = (
