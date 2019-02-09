cbbefbffe223b379c27f6223b03d181fadc3b1ef
G. Eli Jergensen <gelijergensen@ou.edu>
2019-02-09 13:34:50 -0600

    Scoring strategy composition (#68)
    * Added object for scoring strategy composition and fixed old compositions
    
    * Moved version number to 1.2.1.1

--- test/test_scoring_strategies.py
+++ test/test_scoring_strategies.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from PermutationImportance.scoring_strategies import verify_scoring_strategy, VALID_SCORING_STRATEGIES
+from PermutationImportance.scoring_strategies import verify_scoring_strategy, VALID_SCORING_STRATEGIES, argmin_of_mean, indexer_of_converter
 from PermutationImportance.error_handling import InvalidStrategyException
 
 
@@ -17,3 +17,14 @@ def test_invalid_strategy():
 def test_valid_string_strategy():
     for key, value in VALID_SCORING_STRATEGIES.items():
         assert value == verify_scoring_strategy(key)
+
+
+def test_composed():
+    assert 2 == argmin_of_mean([np.array([1, 2]), np.array(
+        [2, 4]), np.array([0, 1]), np.array([10, 12])])
+
+    assert 3 == VALID_SCORING_STRATEGIES['argmax_of_mean']([np.array([1, 2]), np.array(
+        [2, 4]), np.array([0, 1]), np.array([10, 12])])
+
+    assert 2 == indexer_of_converter(np.argmin, np.mean)([np.array([1, 2]), np.array(
+        [2, 4]), np.array([0, 1]), np.array([10, 12])])
